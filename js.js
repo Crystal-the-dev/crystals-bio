609b2858db63d4bed64b9ed3232bcc3ea4c74d66
Crystal <Huskthedev+crystalGit@gmail.com>
2025-12-17 14:53:53 +0000

    modified:   home/index.html 	modified:   js.js 	modified:   style.css

--- js.js
+++ js.js
@@ -1,12 +1,11 @@
 window.addEventListener("load", () => {
     const intro = document.getElementById("intro");
     const content = document.getElementById("content");
-
+    
     if (!intro || !content) return;
-
+    
     setTimeout(() => {
         intro.classList.add("fade-out");
-
         setTimeout(() => {
             intro.style.display = "none";
             content.classList.add("fade-in");
